fffc80c01cc63d9dfd717bfa6b6c53b28bbf5da0
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-15 15:19:44 -0400

    wrote orm.js file

--- config/orm.js
+++ config/orm.js
@@ -1,4 +1,103 @@
-// Dependencies
-const connection = require("./connection");
+// Import MySQL
+var connection = require("../config/connection.js");
 
-// module.exports = orm;
+function printQuestionMarks(num) {
+  var arr = [];
+
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+
+  return arr.toString();
+}
+
+// Converts object key/value pairs into SQL syntax
+function objToSql(ob) {
+  var arr = [];
+
+  // Loop through the keys and push the values into the arr array
+  for (var key in ob) {
+    var value = ob[key];
+    // check to skip hidden properties
+    if (Object.hasOwnProperty.call(ob, key)) {
+      // If a string has spaces, add quotations
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+
+  // Turn all the strings in the array into one long string
+  return arr.toString();
+}
+
+// Object containing all SQL function statements
+var orm = {
+  // Display all data in the burgers table
+  all: function (tableInput, cb) {
+    var queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+      cb(result);
+    });
+  },
+  // Insert a new value
+  create: function (table, cols, vals, cb) {
+    var queryString = "INSERT INTO " + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printQuestionMarks(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  // Update an existing value
+  update: function (table, objColVals, condition, cb) {
+    var queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += objToSql(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+  //   Delete an existing value
+  delete: function (table, condition, cb) {
+    var queryString = "DELETE FROM " + table;
+    queryString += " WHERE ";
+    queryString += condition;
+
+    connection.query(queryString, function (err, result) {
+      if (err) {
+        throw err;
+      }
+
+      cb(result);
+    });
+  },
+};
+
+// Export the orm
+module.exports = orm;
