f757baac2fdea978302e6ff9efdcd2ac67c8032a
frillic <69489498+crowe828@users.noreply.github.com>
2020-10-17 18:24:53 -0400

    added JawsDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,19 @@
 // MySQL connection
+const e = require("express");
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Xgeuaot1!",
-  database: "burgers_db",
-});
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "Xgeuaot1!",
+    database: "burgers_db",
+  });
+}
 
 // Connect to the db
 connection.connect(function (err) {
